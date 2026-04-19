04ca1d830bec35307681e4ca83ffe19bf22c208a
rhettpilcher <112765849+rhettpilcher@users.noreply.github.com>
2026-04-19 19:33:34 -0400

    Screen Map Setup

---
+++ app/src/main/java/edu/uga/cs/roommateapp/fragments/RecentlyPurchasedFragment.java
@@ -0,0 +1,18 @@
+package edu.uga.cs.roommateapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import edu.uga.cs.roommateapp.R;
+
+public class RecentlyPurchasedFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recently_purchased, container, false);
+    }
+}
